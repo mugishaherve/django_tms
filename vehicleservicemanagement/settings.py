c6949583b86cdf52b1d795f58f9571f127d8dfd0
herve <mugishaherve1000@gmail.com>
2024-11-22 08:45:54 +0200

    first commit

--- vehicleservicemanagement/settings.py
+++ vehicleservicemanagement/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'vehicleservicemanagement.wsgi.application'
 
 DATABASES = {
  'default': {
-     'ENGINE': 'django.db.backends.sqlite3',
-     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Transport',
+        'USER': 'root',
+        'PASSWORD': 'Ikirenga@2024',
+        'HOST': 'localhost',
+        'PORT': '3306', 
  }
 }
 
